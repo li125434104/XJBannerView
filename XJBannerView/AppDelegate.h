ce9ecdf70d8e5c69727c574df1ce82d3418e8d6a
li125434104 <125434104@qq.com>
2016-12-20 16:03:57 +0800

    Initial Commit

---
+++ XJBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJBannerView
+//
+//  Created by LXJ on 2016/12/20.
+//  Copyright © 2016年 LianLuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJBannerView
+//
+//  Created by LXJ on 2016/12/20.
+//  Copyright © 2016年 LianLuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
